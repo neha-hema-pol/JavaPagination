62ca0706b97de512c5701e82ef7a3a507bc5789d
nehapol138 <119516090+nehapol138@users.noreply.github.com>
2023-09-02 22:26:57 +0530

    Add files via upload

---
+++ src/main/java/com/javatechie/jpa/dto/APIResponse.java
@@ -0,0 +1,17 @@
+package com.javatechie.jpa.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+public class APIResponse<T> {
+
+    int recordCount;
+    T response;
+
+    public APIResponse(int size, T productsWithPagination) {
+    }
+}
